cde88121dac09ee80bccb097bc6aecfd3a29e9be
Amrindra Ouk <amrindraouk@yahoo.com>
2023-03-27 00:16:50 -0400

    Added .env file

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: `9e549586162346af8976ac9e8746c40c`
+        key: import.meta.env.VITE_API_KEY
     }
 })
